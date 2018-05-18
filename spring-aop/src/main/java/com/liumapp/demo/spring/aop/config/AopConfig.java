7e53455eb226b6e1fab15d21ba3e9d186a6f3a8a
liumapp <liumapp.com@gmail.com>
2018-05-18 17:12:50 +0800

    add aop config

--- spring-aop/src/main/java/com/liumapp/demo/spring/aop/config/AopConfig.java
+++ spring-aop/src/main/java/com/liumapp/demo/spring/aop/config/AopConfig.java
@@ -1,5 +1,10 @@
 package com.liumapp.demo.spring.aop.config;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 /**
@@ -10,8 +15,20 @@ import org.springframework.stereotype.Component;
  * @date 5/18/18
  */
 @Component
+@Aspect
 public class AopConfig {
 
+    @Pointcut("execution(* com.liumapp.demo.spring.aop.*(..))")
+    public void pointCut () {};
 
+    @Before("pointCut()")
+    public void beginTime () {
+
+    }
+
+    @After("pointCut()")
+    public void afterTime () {
+
+    }
 
 }
