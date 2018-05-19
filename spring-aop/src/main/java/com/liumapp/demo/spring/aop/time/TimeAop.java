65f3a22b29d09c5359d79e12934dea0cc5b558ae
liumapp <liumapp.com@gmail.com>
2018-05-19 10:14:25 +0800

    aop test success

--- spring-aop/src/main/java/com/liumapp/demo/spring/aop/time/TimeAop.java
+++ spring-aop/src/main/java/com/liumapp/demo/spring/aop/time/TimeAop.java
@@ -4,7 +4,7 @@ import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.annotation.Pointcut;
-import org.springframework.context.annotation.Bean;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -16,21 +16,22 @@ import java.util.Date;
  * @homepage http://www.liumapp.com
  * @date 5/18/18
  */
+@Order(1)
 @Component
 @Aspect
 public class TimeAop {
 
-    @Pointcut("execution(* com.liumapp.demo.spring.aop.*(..))")
-    public void pointCut () {};
+    @Pointcut("execution(* com.liumapp.demo.spring.aop.human.Hunaness.eat(..))")
+    public void eat () {}
 
-    @Before("pointCut()")
+    @Before("eat()")
     public void beginTime () {
-        System.out.println("now is : " + new Date());
+        System.out.println("eat begin at : " + new Date());
     }
 
-    @After("pointCut()")
+    @After("eat()")
     public void afterTime () {
-        System.out.println("now is : " + new Date());
+        System.out.println("eat end at : " + new Date());
     }
 
 }
---
+++ spring-aop/src/main/java/com/liumapp/demo/spring/aop/time/TimeAopMain.java
@@ -0,0 +1,25 @@
+package com.liumapp.demo.spring.aop.time;
+
+import com.liumapp.demo.spring.aop.config.Config;
+import com.liumapp.demo.spring.aop.human.Human;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+/**
+ * @author liumapp
+ * @file TimeAopMain.java
+ * @email liumapp.com@gmail.com
+ * @homepage http://www.liumapp.com
+ * @date 5/19/18
+ */
+public class TimeAopMain {
+
+    public static void main (String[] args) {
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(Config.class);
+        context.refresh();
+        Human human = context.getBean(Human.class);
+        human.eat();
+        context.close();
+    }
+
+}
